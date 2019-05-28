5d952f0d6a781e5670ad8fcd4949fbe7d2d1c2e8
kudirata <kudirata@yahoo.com>
2019-05-28 19:56:32 -0400

    /JavaProgrammingSpring2019

---
+++ src/day52_InheritanceOverriding/EqualsMethod.java
@@ -0,0 +1,22 @@
+package day52_InheritanceOverriding;
+
+public class EqualsMethod {
+	public static void main(String[] args) {
+		Computer comp1 = new Computer("iMac", "silver");
+		Computer comp2 = new Computer("iMac", "silver");
+		Computer comp3 = comp2;//point to same object
+
+		System.out.println(comp1 == comp2);
+		System.out.println(comp1.equals(comp2));
+
+		System.out.println(comp3 == comp2);
+		System.out.println(comp3.equals(comp2));
+
+		String str1 = "java";
+		String str2 = new String("java");
+
+		System.out.println(str1 == str2); // false
+		System.out.println(str1.equals(str2)); //true
+
+	}
+}
---
+++ src/day52_InheritanceOverriding/SubObject.java
@@ -0,0 +1,13 @@
+package day52_InheritanceOverriding;
+
+public class SubObject extends Object{
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SubObject sb = new SubObject();
+		sb.toString();
+	}
+
+}
